5587bc6273cf214a6f16216f331720c271ac98fc
pseudoincorrect <maxime-clement@live.fr>
2018-05-23 18:21:32 +0300

    before refactoring DB

--- back_end/vidly_6_errors_handling/index.js
+++ back_end/vidly_6_errors_handling/index.js
@@ -2,15 +2,11 @@ const config = require('config');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+require('./startup/routes')(app);
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-// routes
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const authentication = require('./routes/authentication');
+winston = require('winston');
+require('winston-mongodb');
 
 // powershell: $env:vidly_jwtPrivateKey = "mySecureKey"
 // powershell: Write-Output $env:vidly_jwtPrivateKey
@@ -23,13 +19,25 @@ mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/authentication', authentication);
+  winston.handleExceptions(
+    new winston.transports.Console({colorize: true, prettyPrint: true}),
+    new winston.transports.File({filename: "exception.log"})
+  );
+  
+  process.on('unhandledRejection', (ex) => {
+    // since winston does not catch unhandled Rejections,
+    // we catch them with the OS event (process.on())
+    // and throw it as an exception that will be caught
+    // by winston
+    throw ex;
+  });
+  
+  winston.add(winston.transports.File,{filename:'logfile.log'});
+  winston.add(winston.transports.MongoDB, {
+    db: 'mongodb://localhost/vidly',
+    level: "info"
+  });
+  
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
