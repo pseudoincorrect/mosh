e3536a669dd7859afdc165020805504ef08c1213
pseudoincorrect <maxime-clement@live.fr>
2018-06-06 17:05:58 +0300

    form

--- front_end/formz/src/app/temp-driv-form/temp-driv-form.component.ts
+++ front_end/formz/src/app/temp-driv-form/temp-driv-form.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TempDrivFormComponent implements OnInit {
 
+  categories = [
+    {id:1, name:"category 1"},
+    {id:2, name:"category 2"},
+    {id:3, name:"category 3"},
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
