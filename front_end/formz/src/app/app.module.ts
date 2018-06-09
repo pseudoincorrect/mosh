95b3f5d60e6b06c7afde0645edd8869ede915df2
Maxime Clement <maxime-clement@live.fr>
2018-06-09 18:31:49 +0300

    end reactive form

--- front_end/formz/src/app/app.module.ts
+++ front_end/formz/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TempDrivFormComponent } from './temp-driv-form/temp-driv-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TempDrivFormComponent
+    TempDrivFormComponent,
+    ReactiveFormComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
